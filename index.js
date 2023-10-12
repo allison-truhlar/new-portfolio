45ab49c44656c9c8ec8048e4edcd58303dbcfd8b
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-10-12 16:51:05 -0400

    Trying to fix fade-in appearance on non-home page

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ let isDropdownVisible = false;
 
 // Toggle the dropdown menu on smaller screens and animate the hamburger menu
 menuBtn.addEventListener('click', () => {
-    if (isDropdownVisible){
+    if (isDropdownVisible) {
         menuBtn.classList.remove("change")
         dropdown.style.display = "none"
         isDropdownVisible = false
-    } else if (!isDropdownVisible){
+    } else if (!isDropdownVisible) {
         menuBtn.classList.add('change');
         dropdown.style.display = "flex"
         isDropdownVisible = true
@@ -19,19 +19,67 @@ menuBtn.addEventListener('click', () => {
 });
 
 
-// Code to watch for when a div with class appear enters the screen
+// Code to watch for when a div with class home-appear enters the screen
 // Following this article: https://dev.to/miacan2021/fade-in-animation-on-scroll-with-intersectionobserver-vanilla-js-4p27
-const appearItems = document.querySelectorAll('.appear'); 
-const callback = function(entries){
-  entries.forEach(entry => {
-    if(entry.isIntersecting){
-      entry.target.classList.add('inview');
-    }else if (entry.isIntersecting && entry.target.classList.contains('inview')){
-        entry.unobserve(entry.target);
-    }
-  });
+const homeAppearItems = document.querySelectorAll('.home-appear');
+
+const delayHomeAppearCallback = function (entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('delay-inview');
+        } else if (entry.isIntersecting && entry.target.classList.contains('delay-inview' || 'inview')) {
+            entry.unobserve(entry.target);
+        }
+    });
 }
-const intersectionObserver = new IntersectionObserver(callback);
-for(let i=0; i < appearItems.length; i++){
-    intersectionObserver.observe(appearItems[i]);
- }
+
+const homeAppearCallback = function (entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('inview');
+        } else if (entry.isIntersecting && entry.target.classList.contains('delay-inview' || 'inview')) {
+            entry.unobserve(entry.target);
+        }
+    });
+}
+
+const delayHomeSectionsObserver = new IntersectionObserver(delayHomeAppearCallback);
+const homeSectionsObserver = new IntersectionObserver(homeAppearCallback);
+
+// Check if home-hero class exists before using homeHeroObserver
+const homeHero = document.querySelector(".home-hero");
+if (homeHero) {
+    const homeCallback = function (entries) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                console.log("Home hero is intersecting the viewport");
+                for (let i = 0; i < homeAppearItems.length; i++) {
+                    delayHomeSectionsObserver.observe(homeAppearItems[i]);
+                }
+            } else if (!entry.isIntersecting) {
+                console.log("Home hero is NOT intersecting the viewport");
+                for (let i = 0; i < homeAppearItems.length; i++) {
+                    homeSectionsObserver.observe(homeAppearItems[i]);
+                }
+            }
+        });
+    };
+    const homeHeroObserver = new IntersectionObserver(homeCallback);
+    homeHeroObserver.observe(homeHero);
+} else {
+    // If home-hero class is not found, use sectionsObserver for appearItems
+    const appearItems = document.querySelectorAll('.appear');
+    const appearCallback = function (entries) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('inview');
+            } else if (entry.isIntersecting && entry.target.classList.contains('inview')) {
+                entry.unobserve(entry.target);
+            }
+        });
+    }
+    const sectionsObserver = new IntersectionObserver(appearCallback);
+    for (let i = 0; i < appearItems.length; i++) {
+        sectionsObserver.observe(appearItems[i]);
+    }
+}
